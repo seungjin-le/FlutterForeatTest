892a89ebcd4d282fc61fe81e5242c258c6db6676
Lee Seung Jin <dltmdwls154@gmail.com>
2025-02-09 17:58:51 +0900

    Test

---
+++ lib/themes/colors.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color darker = Color(0xFF192B59);
+  static const Color orange = Color(0xFFFFA800);
+  static const Color gray = Color(0xFFFAFAFC);
+  static const Color mGray = Color(0xFFF9F9F9);
+  static const Color error = Color(0xFFFF3257);
+
+  static const Color n10 = Color(0xFF151B1E);
+  static const Color n20 = Color(0xFFEAEAEA);
+  static const Color n30 = Color(0xFFDFDEDE);
+  static const Color n40 = Color(0xFFC8C8C8);
+  static const Color n50 = Color(0xFFAAAAAA);
+  static const Color n60 = Color(0xFF848484);
+  static const Color n70 = Color(0xFF666666);
+  static const Color n80 = Color(0xFF3D3D3D);
+  static const Color n90 = Color(0xFF212121);
+
+  static const Color s50 = Color(0xFFF6F9FF);
+  static const Color s100 = Color(0xFFEDF2FF);
+  static const Color s200 = Color(0xFFEAEAEA);
+  static const Color s300 = Color(0xFFC6D6FF);
+  static const Color s400 = Color(0xFF487AFF);
+  static const Color s500 = Color(0xFF416EE6);
+  static const Color s600 = Color(0xFF3A62CC);
+  static const Color s700 = Color(0xFF365CBF);
+  static const Color s800 = Color(0xFF2B4999);
+  static const Color s900 = Color(0xFF203773);
+
+  static const Color p50 = Color(0xFFFFFCF8);
+  static const Color p100 = Color(0xFFFFF3DD);
+  static const Color p200 = Color(0xFFFFDC99);
+  static const Color p300 = Color(0xFFFFCB66);
+  static const Color p400 = Color(0xFFFFBA33);
+  static const Color p500 = Color(0xFFFFA800);
+  static const Color p600 = Color(0xFFCC8700);
+  static const Color p700 = Color(0xFF996500);
+  static const Color p800 = Color(0xFF664300);
+  static const Color p900 = Color(0xFF332200);
+
+  static const Color pink_50 = Color(0xFFFFF3DD);
+  static const Color pink_100 = Color(0xFFFFF3DD);
+  static const Color pink_200 = Color(0xFFFFDC99);
+  static const Color pink_300 = Color(0xFFFFCB66);
+  static const Color pink_400 = Color(0xFFFFBA33);
+  static const Color pink_500 = Color(0xFFFFA800);
+  static const Color pink_600 = Color(0xFFCC8700);
+  static const Color pink_700 = Color(0xFF996500);
+  static const Color pink_800 = Color(0xFF664300);
+  static const Color pink_900 = Color(0xFF332200);
+}
---
+++ lib/themes/text.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle h1 = TextStyle(
+    fontSize: 44,
+    fontWeight: FontWeight.w500,
+    height: 1,
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontSize: 36,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+
+  static const TextStyle h3 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+
+  static const TextStyle h4 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    height: 1.2,
+  );
+
+  static const TextStyle h5 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+
+  static const TextStyle l1 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+
+  static const TextStyle l2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
+  );
+
+  static const TextStyle m1 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    height: 1.4,
+  );
+
+  static const TextStyle m2 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 1.1,
+  );
+
+  static const TextStyle n1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 1.6,
+  );
+
+  static const TextStyle n2 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.6,
+  );
+
+  static const TextStyle s1 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 1.4,
+    letterSpacing: 0.5,
+  );
+  static const TextStyle s2 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.4,
+  );
+  static const TextStyle xs1 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    height: 1,
+  );
+  static const TextStyle xs2 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+}
