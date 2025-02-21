0c513021fee341e91f9cffa92d0f731442983271
Lee Seung Jin <dltmdwls154@gmail.com>
2025-02-21 23:04:16 +0900

    Test

--- lib/bindings/login_binding.dart
+++ lib/bindings/login_binding.dart
@@ -1,5 +1,5 @@
 import 'package:foreat_mobile/controllers/login_controller.dart';
-import 'package:foreat_mobile/services/home_service.dart';
+import 'package:foreat_mobile/services/login_service.dart';
 import 'package:get/get.dart';
 
 class LoginBinding implements Bindings {
