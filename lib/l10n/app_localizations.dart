5ff075f6c530ae366455514c84b86a0b52a1096d
Lee Seung Jin <dltmdwls154@gmail.com>
2025-02-26 23:44:22 +0900

    Test

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
 
 class AppLocalizations {
+  // 지원되는 로케일 목록 정의
   static const supportedLocales = [
+    // 한국어 로케일
     Locale('ko', 'KR'),
+    // 영어 로케일
     Locale('en', 'US'),
   ];
 }
