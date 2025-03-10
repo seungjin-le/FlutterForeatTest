94fa7b10b83601ecc60c7603eeecffe8fe04d2b0
Lee Seung Jin <dltmdwls154@gmail.com>
2025-03-10 22:42:07 +0900

    Test

---
+++ lib/vo/menu_vo.dart
@@ -0,0 +1,16 @@
+class MenuVo {
+  final String title;
+  final String image;
+  final String description;
+  final String price;
+  final String category;
+  final String id;
+
+  MenuVo(
+      {required this.title,
+      required this.image,
+      required this.description,
+      required this.price,
+      required this.category,
+      required this.id});
+}
